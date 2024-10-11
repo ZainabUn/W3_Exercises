b4fe850651a5b0bc1340414638700726bd129290
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-11 15:53:21 -0500

    Seeing if W3_Workshop created as folder in github

---
+++ W3_Workshop/question1.sql
@@ -0,0 +1,3 @@
+-- 1. Display the first and last name of each actor in a single column in upper
+-- case letters. Name the column in the results Actor Name.
+-- + Create an additional query to sort Actor Name by the actor’s last name.
